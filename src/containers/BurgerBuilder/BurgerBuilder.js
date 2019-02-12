9958316a4c0bb65d2a3b9e10b39de935c6a62279
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-02-12 12:52:07 -0500

    able to add burger ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger.js'
+import BuildControls from '../../components/BuildControls/BuildControls.js'
 
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    addIngredientHandler = (type) => {
+        let ingredientsCopy = {...this.state.ingredients}
+        ingredientsCopy[type] += 1
+        this.setState({ingredients: ingredientsCopy})
+    }
+
+    removeIngredientHandler = (type) => {
+        let ingredientsCopy = {...this.state.ingredients}
+        ingredientsCopy[type] = Math.max(0, ingredientsCopy[type] - 1)
+        this.setState({ingredients: ingredientsCopy})
+    }
+
     render () {
         return (
             <Aux>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                    lessPressed={this.removeIngredientHandler}
+                    morePressed={this.addIngredientHandler}
+                />
             </Aux>
 
         );

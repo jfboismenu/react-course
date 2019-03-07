747f633c27bc3a6b1b75060d8cdb548ae65e9697
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-03-07 09:41:58 -0500

    witching to chapter 9 ocntent. burger builder has been moved to the burger_builder branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <BurgerBuilder/>
-      </Layout>
+      <div className="App">
+        <Blog />
+      </div>
     );
   }
 }
